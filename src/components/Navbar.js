7f9646d5ce31a63de054dea123b76462b6e41f5c
tinemanabs <christinemmanabat@gmail.com>
2023-03-22 17:59:25 +0800

    link change & title page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
     return (
         <nav className="px-4 sm:px-9 py-2.5 bg-blue-900">
             <div className="container mx-auto flex items-center justify-between">
-                <a href="/" id="logo" className="text-5xl text-white">cm</a>
+                <Link to="/" id="logo" className="text-5xl text-white">cm</Link>
                 <button className="inline-flex md:hidden text-white hover:bg-gray-50 hover:text-gray-500 rounded-md" onClick={menuBtnHandler}>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd">
